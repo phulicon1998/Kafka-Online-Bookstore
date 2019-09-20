78ea47ed8f5f947a545e0aca2b888542240c0b0e
phulicon1998 <phulicon815490@gmail.com>
2019-09-20 18:24:44 +0700

    Add all the component for the dashboard

--- cli/src/views/App/index.jsx
+++ cli/src/views/App/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import {Switch, Redirect, Route, withRouter} from "react-router-dom";
-import Dashboard from "./Dashboard";
+import Dashboard from "./CRM/index";
 
 function AppRoutes({match, location}) {
     return (
-        <Switch>
-            <Route path={`${match.url}/dashboard`} component={Dashboard}/>
-            <Redirect from={location.pathname} to={`${match.url}/dashboard`}/>
-        </Switch>
+        <div className="gx-main-content-wrapper">
+            <Switch>
+                <Route path={`${match.url}/dashboard`} component={Dashboard}/>
+                <Redirect from={location.pathname} to={`${match.url}/dashboard`}/>
+            </Switch>
+        </div>
     )
 }
 
