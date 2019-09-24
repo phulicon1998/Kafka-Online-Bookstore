938ead813cdc4ece44fe97ed575191ef0f00445d
phulicon1998 <phulicon815490@gmail.com>
2019-09-24 17:32:10 +0700

    Working on design for auth page

--- cli/src/views/Auth/Auth.jsx
+++ cli/src/views/Auth/Auth.jsx
@@ -1,21 +1,87 @@
 import React from "react";
 
+const SocialBtn = ({name, icon, color}) => (
+    <button
+        className="social-btn"
+        style={{
+            "backgroundColor": `${color}`,
+            "borderColor": `${color}`
+        }}
+    >
+        <i className={icon}/>
+        <span>{name}</span>
+    </button>
+)
+
+const AuthInput = ({icon, type, ...input}) => (
+    <div className="auth-input">
+        <div className="form-group">
+            <div className="input-group">
+                <div className="input-group-addon">
+                    <i className={icon}/>
+                </div>
+                <input type={type ? type : "text"} {...input}/>
+            </div>
+        </div>
+    </div>
+)
+
 function Auth() {
     return (
-        <div className="auth-content">
-            <div className="container">
-                <div class="intro-content">
-    				<h1 class="name-appear"><i class="fas fa-book"></i> Kafka</h1>
-    				<p class="slogan-appear">Book for everyone</p>
-    				<p class="beMember-appear">Don't have account? Want to be a member?</p>
-    				<button class="beMember-appear">Join Us</button>
-    				<p class="social-appear">Or login via social account</p>
-    				<div>
-    					<button class="facebook-appear"><i class="fab fa-facebook-f"></i> <span>Facebook</span></button>
-    					<button class="twitter-appear"><i class="fab fa-twitter"></i> <span>Twitter</span></button>
-    					<button class="google-appear"><i class="fab fa-google"></i> <span>Google</span></button>
-    				</div>
-    			</div>
+        <div className="container">
+            <div className="auth-content">
+                <div id="intro">
+                    <h1><i className="fas fa-book"/> Kafka</h1>
+                    <p>Book for everyone</p>
+                    <p>Don't have account? Want to be a member?</p>
+                    <button>Join Us</button>
+                    <p>Or login via social account</p>
+                    <div>
+                        <SocialBtn
+                            name="Facebook"
+                            icon="fab fa-facebook-f"
+                            color="#496b9c"
+                        />
+                        <SocialBtn
+                            name="Twitter"
+                            icon="fab fa-twitter"
+                            color="#4eb1ab"
+                        />
+                        <SocialBtn
+                            name="Google"
+                            icon="fab fa-google"
+                            color="#b74c2f"
+                        />
+                    </div>
+                </div>
+                <div id="form"> {/*content right appear*/}
+                    <div>
+                        <h2>Login</h2>
+                        <p>Enter your username and password to log on.</p>
+                        <hr/>
+                        <div>
+                            <AuthInput
+                                icon="fas fa-envelope"
+                                placeholder="Email"
+                                name="email"
+                            />
+                            <AuthInput
+                                type="password"
+                                icon="fas fa-key"
+                                placeholder="Password"
+                                name="password"
+                            />
+                            {/* <AuthInput
+                                type="password"
+                                icon="fas fa-key"
+                                placeholder="Password"
+                                name="confirmPassword"
+                            /> */}
+                            <button className="btn btn-primary btn-block button-signup btnSignup-appear">Sign in</button>
+                        </div>
+                        <a href="/">Forgot your password?</a>
+                    </div>
+                </div>
             </div>
         </div>
     )
