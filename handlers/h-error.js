080474437e58406696eba5605b3467987036f7f5
Phú Nguyễn <31927961+phunguyentan109@users.noreply.github.com>
2020-07-11 09:17:25 +0700

    Finish, put in maintenance

--- handlers/h-error.js
+++ handlers/h-error.js
@@ -1,5 +1,4 @@
-exports.handle = (err, req, res, next) => {
-    console.log(err);
+exports.handle = (err, req, res) => {
     return res.status(err.status || 500).json({
         errorMsg: err.message || "Oops! Something went wrong!"
     })
